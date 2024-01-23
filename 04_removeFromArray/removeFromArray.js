50e5a019a58368802acb47fc6b6351fa213306ed
Jancee Rod <jancee800@gmail.com>
2024-01-23 18:42:35 +0800

    add decrement to reset for loop when something is removed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,6 +3,7 @@ const removeFromArray = function(array, ...removeArguments) {
         for (j = 0; j < removeArguments.length; j++) {
             if (array[i] === removeArguments[j]) {
                 array.splice(i, 1);
+                i--;
             }
         }
     }
