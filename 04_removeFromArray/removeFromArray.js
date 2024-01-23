d8fc21fedacb7b8054f4ecb134328d9f3b0e21b7
Jancee Rod <jancee800@gmail.com>
2024-01-23 18:34:12 +0800

    create two for loop that goes through each element of array and arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeArguments) {
+    for (i = 0; i < array.length; i++) {
+        for (j = 0; j < removeArguments.length; j++) {
+            if (array[i] === removeArguments[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
