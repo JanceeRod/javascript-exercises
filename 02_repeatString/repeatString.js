da4cbbc9fb417743ad304da21fd5ebbe40f9b5cc
Jancee Rod <jancee800@gmail.com>
2024-01-23 17:49:35 +0800

    create for-loop and return repeated word, passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text, number) {
+    let result ="";
 
+    if (number >= 0) {
+        for (i = 0; i < number; i++) {
+            result += text;
+        }
+    }
+    else {
+        result = "ERROR";
+    }
+
+    return result;
 };
 
 // Do not edit below this line
