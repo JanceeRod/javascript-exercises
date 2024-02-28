5a739d728d32452e5ef569104fd6ed12d8178c62
Jancee Rod <jancee800@gmail.com>
2024-02-28 16:02:36 +0800

    finish

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let firstDigit = 0;
+    let secondDigit = 1;
+    let fiboNumber = 0;
 
+    if (number < 0) return "OOPS"
+    if (number == 1) return 1
+
+    for (i = 1; i < String(number); i++) {
+        fiboNumber = firstDigit + secondDigit
+        firstDigit = secondDigit
+        secondDigit = fiboNumber
+    }
+
+    return fiboNumber
 };
 
 // Do not edit below this line
