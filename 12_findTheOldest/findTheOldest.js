913d45b99c04e2c7097e90dd09bbc2d124165c7c
Jancee Rod <jancee800@gmail.com>
2024-02-28 16:56:53 +0800

    finish

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentOld = people[0]
 
+    for (let person of people) {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear()
+        }
+
+        const first = person.yearOfDeath - person.yearOfBirth
+        const next = currentOld.yearOfDeath - currentOld.yearOfBirth
+
+        if (first > next) {
+            currentOld = person
+        }
+    }
+    return currentOld
 };
 
 // Do not edit below this line
